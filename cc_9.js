c4f9ba886267240a53fac6ef9e6e5db22e309358
Derek Ruize <derekruiz@usf.edu>
2025-02-23 22:15:27 -0500

    Task 2 - Created Manager Class with Inheritance / Fixed Error from Task 1

--- cc_9.js
+++ cc_9.js
@@ -10,7 +10,7 @@ class Employee { //Creating Class
         return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`;
     }
     calculateAnnualSalary() { //Creating a method to return employee annual salary
-        return `this.salary * 12`
+        return this.salary * 12
     }
 }
 
@@ -20,3 +20,21 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 console.log(emp1.getDetails()); 
 
 console.log(emp1.calculateAnnualSalary()); 
+
+//Task 2 - Creating A Manager Class
+class Manager extends Employee{ //Making Manager Class
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary),
+        this.teamSize = teamSize
+    }
+    getDetails(){ //Updating method to include team size
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }
+    calculateBonus(){ //Calculating Manager Bonus
+        return (this.salary * 12) * 0.1
+    }
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus()); 
